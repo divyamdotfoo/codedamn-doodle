115574c59dcf1dd30ea6c6bbb5d30a6f95896b13
divyam gupta <divyam7091@gmail.com>
2024-01-15 11:42:30 +0530

    fix

--- src/components/birthday.tsx
+++ src/components/birthday.tsx
@@ -3,5 +3,6 @@ import { api } from "@/server/react";
 import { useState } from "react";
 export function Birthday() {
   // your code goes here
+  const birthdayMutation = api.message.birthday.useMutation();
   return <p>delete this to get started</p>;
 }
